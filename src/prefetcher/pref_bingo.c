10029af6d431a73d211ed725ae530268ffb58998
Mohit Agrawal <himohita@gmail.com>
2024-12-04 22:02:43 -0800

    debug prints

--- src/prefetcher/pref_bingo.c
+++ src/prefetcher/pref_bingo.c
@@ -91,17 +91,14 @@ void pref_bingo_ul1_hit(uns8 proc_id, Addr lineAddr, Addr loadPC, uns32 global_h
 
   Bingo_Table_Line* line = hash_table_access(&History_Table, pc_plus_offset);
   Bingo_History_Table* hash_entry = pref_bingo_find_event_to_fetch_addr(line, pc_plus_address);
-   return;
   if (hash_entry == NULL){
     hash_entry =  pref_bingo_find_event_to_fetch(line, pc_plus_offset);
-     return;
   }
   int block = (block_address & 0x3F);
   if (hash_entry == NULL){
     Aux_Entry* aux_entry = hash_table_access(&Aux_Storage, page_address);
      return;
     if (aux_entry){
-       return;
       aux_entry->footprint.accessed[block] = TRUE;
        return;
     }
