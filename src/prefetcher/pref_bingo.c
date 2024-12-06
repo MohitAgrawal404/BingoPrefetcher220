448da6c175e6c7bb8d50e398c9619d40f5313f5f
Mohit Agrawal <himohita@gmail.com>
2024-12-06 13:56:19 -0800

    increasing table size

--- src/prefetcher/pref_bingo.c
+++ src/prefetcher/pref_bingo.c
@@ -73,7 +73,7 @@ void pref_bingo_init(HWP* hwp) {
     return;
   hwp->hwp_info->enabled = TRUE;
   hwp_in = *(hwp->hwp_info);
-  init_hash_table(&History_Table, "History Table", 1024, sizeof(Bingo_Table_Line));
+  init_hash_table(&History_Table, "History Table", 2048, sizeof(Bingo_Table_Line));
   init_hash_table(&Aux_Storage, "Auxiliary Storage", 1024, sizeof(Aux_Entry));
   return;
 }
